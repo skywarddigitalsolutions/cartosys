919d4d8c28cbb281fd9b610819a0716c817909d3
Agustin <agusquiroga648@gmail.com>
2024-12-13 00:04:25 -0300

    cambios

--- app/components/ui/ContactForm.tsx
+++ app/components/ui/ContactForm.tsx
@@ -102,7 +102,7 @@ export function ContactForm() {
             </FormItem>
           )}
         />
-        <Button type="submit" className='bg-verde' disabled={isSubmitting}>
+        <Button type="submit" className='bg-verde text-blanco' disabled={isSubmitting}>
           {isSubmitting ? "Enviando..." : "Enviar mensaje"}
         </Button>
         {submitStatus === 'success' && (
